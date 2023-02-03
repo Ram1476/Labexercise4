fc6e077a3911a1782e6d49216d0c083b278e4fd7
Ram1476 <ramilangovan.1299@gmail.com>
2023-02-03 11:27:16 +0530

    Add project files.

---
+++ Labexercise4/Employee_Details.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labexercise4
+{
+    internal class Employee_details
+    {
+        public int empId;
+        public string EmpName;
+        public int Age;
+        public string EmpDOB;
+        public int Basichra;
+
+        public void emp_details() 
+        {
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"\nEmp_ID: {empId}\n\nEmp_Name: {EmpName}\n\n" +
+                $"Emp_Age: {Age}\n\nEmp_DOB: {EmpDOB}\n\nEmp_Basic_HRA: {Basichra}");
+            Console.WriteLine("---------------------------------------");
+        } 
+    }
+}
---
+++ Labexercise4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labexercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee_details emp1 = new Employee_details(); 
+            Console.Write("Enter Emp_ID: ");
+            emp1.empId = Convert.ToInt32( Console.ReadLine());
+            Console.Write("\nEnter Emp_Name: ");
+            emp1.EmpName = Console.ReadLine();
+            Console.Write("\nEnter Emp_Age:");
+            emp1.Age = Convert.ToInt32( Console.ReadLine());
+            Console.Write("\nEnter Emp_DOB: ");
+            emp1.EmpDOB =  Console.ReadLine();
+            Console.Write("\nEnter Emp_Basic_HRA: ");
+            emp1.Basichra = Convert.ToInt32( Console.ReadLine());
+
+            emp1.emp_details();
+
+            Console.ReadLine();
+        }
+    }
+}
